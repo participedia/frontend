26f3274c12ac48596bcc7f1f28dd8ae8edc8d7b7
David Ascher <david.ascher@gmail.com>
2017-03-01 22:03:10 -0800

    Use isomrophic fetch

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 // This is the JS API to talk to api.participedia.xyz
 
+fetch = require('isomorphic-fetch');
+
 let APIURL = process.env.REACT_APP_API_URL  // eslint-disable-line no-undef
 
 if (! APIURL) {
