84e00f646b84da22f7ec7347dd22446896ae851d
andreadelrio <delrio.andre@gmail.com>
2017-03-19 21:19:41 -0700

    adding support for portrait images

--- src/containers/Case/ItemGallery.js
+++ src/containers/Case/ItemGallery.js
@@ -3,6 +3,52 @@ import ImageGallery from 'react-image-gallery';
 
 class ItemGallery extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.defineImage = this.defineImage.bind(this);
+    this.getWidth = this.getWidth.bind(this);
+    this.renderItem = this.renderItem.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.loadGalleryPhotos();
+  }
+
+
+  defineImage(url){
+    if (url){
+      let width = this.getWidth(url);
+      if (width < 500) {
+        return ('portrait');
+      } else {
+        return ('landscape');
+      }
+    } else {
+      return;
+    }
+  }
+
+  getWidth(url){
+    var img = new Image();
+    img.src = url;
+    return img.naturalWidth
+  }
+
+  renderItem(item) {
+    const imageClass = this.defineImage(item.original);
+    return (
+      <div className='image-gallery-image'>
+        <img
+          className={imageClass}
+          src={item.original}
+          alt={item.originalAlt}
+          srcSet={item.srcSet}
+          sizes={item.sizes}
+        />
+      </div>
+    );
+  }
+
   render() {
 
     const images = this.props.items.map((photo) => {
@@ -17,6 +63,7 @@ class ItemGallery extends React.Component {
       <ImageGallery
         items={images}
         showFullscreenButton={false}
+        renderItem={this.renderItem}
         slideInterval={2000} />
     );
   }
