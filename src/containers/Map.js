ae9355b83e76f7baa08f6e0cc2fe363ab16ce2d0
David Ascher <david.ascher@gmail.com>
2017-04-26 15:02:18 -0700

    Smoother rendering.

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -3,17 +3,16 @@ import api from "../utils/api";
 import MapVisualization from "../components/MapVisualization/MapVisualization";
 
 export default class Map extends Component {
+  state = {
+    cases: [],
+    orgs: []
+  };
   componentWillMount() {
     api.searchMapTokens().then(results => this.setState(results));
   }
 
   render() {
-    if (!this.state) return <div>LOADING MAP DATA</div>;
-    return (
-      <MapVisualization
-        cases={this.state.cases}
-        organizations={this.state.orgs}
-      />
-    );
+    let { cases, orgs } = this.state;
+    return <MapVisualization cases={cases} organizations={orgs} />;
   }
 }
