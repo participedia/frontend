a55c45213ac5f779cd7f896a81120c07d0ab0782
andreadelrio <delrio.andre@gmail.com>
2017-05-26 13:50:13 -0700

    using only certain parts of bootstrap

--- src/Layout.js
+++ src/Layout.js
@@ -44,7 +44,8 @@ import menuIcon from "./img/menu-icon.png";
 import ppLogo from "./img/pp-logo.png";
 import myhistory from "./utils/history";
 
-import "bootstrap/dist/css/bootstrap.min.css"; // XXX this is maybe avoidable by using reactstrap?
+// import "bootstrap/dist/css/bootstrap.min.css"; // XXX this is maybe avoidable by using reactstrap?
+import "./UniversalStyles.css"
 
 function onSearch(pathname) {
   return pathname === "/" || pathname === "/search";
