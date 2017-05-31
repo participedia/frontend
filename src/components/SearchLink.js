f4822b82b014d14be61053d51cc0985b224239ec
David Ascher <david.ascher@gmail.com>
2017-05-30 23:01:32 -0700

    work around snapshot wonkiness

--- src/components/SearchLink.js
+++ src/components/SearchLink.js
@@ -4,17 +4,20 @@ import { Link } from "react-router-dom";
 export default class SearchLink extends React.Component {
   render() {
     let { tag, value, intl } = this.props;
-    console.log("VALUE", value);
-    console.log("href", encodeURI(value));
+    // console.log("VALUE", value);
+    // console.log("href", encodeURI(value));
+    let href = `/search?${tag}=${value}`;
     return tag && value && intl
-      ? <Link
+      ? <Link to={{ pathname: href }}>{value}</Link>
+      : <div>{intl.formatMessage({ id: "not_specified" })}</div>;
+  }
+}
+
+/* ? <Link
           to={{
             pathname: "/search",
             search: `?${tag}=${value}`
           }}
         >
           {value}
-        </Link>
-      : <div>{intl.formatMessage({ id: "not_specified" })}</div>;
-  }
-}
+        </Link>*/
