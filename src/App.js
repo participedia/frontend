476141f18939d8fd393a2a41574773239ab3c251
David Ascher <david.ascher@gmail.com>
2017-06-17 16:46:35 -0700

    Add raven in a webpacky way.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ import authService from "./utils/AuthService";
 import { getFirstBrowserLanguage, getBestMatchingMessages } from "./utils/l10n";
 import { BrowserRouter } from "react-router-dom";
 import history from "./utils/history";
-try {
-  Raven.config(
-    "https://0189b4df0dd54b6fa22f6202b7fb6c11@sentry.io/180779"
-  ).install();
-} catch (e) {
-  console.log("Got exception starting Raven", e);
-}
+
+import Raven from "raven-js";
+
+Raven.config(
+  "https://0189b4df0dd54b6fa22f6202b7fb6c11@sentry.io/180779"
+).install();
 
 addLocaleData([...en, ...fr, ...es]);
 
