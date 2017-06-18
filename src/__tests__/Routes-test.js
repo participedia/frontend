5a923e6efad4a0a741c73b57bcfa10e10c7b26c4
David Ascher <david.ascher@gmail.com>
2017-06-17 23:20:22 -0700

    duh

--- src/__tests__/Routes-test.js
+++ src/__tests__/Routes-test.js
@@ -16,9 +16,6 @@ import App from "../App";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 const muiTheme = getMuiTheme({});
-jest.mock("raven-js", () => ({
-  install: function() {}
-}));
 
 let fetchMock = require("fetch-mock");
 jest.mock("../containers/Map", () => "Map");
