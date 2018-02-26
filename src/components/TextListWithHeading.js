3f0f7c684fab5d116ca08f7a1de087b7df6a51c5
dethe <dethe@livingcode.org>
2018-02-26 04:34:24 -0800

    Fixed crashing bug

--- src/components/TextListWithHeading.js
+++ src/components/TextListWithHeading.js
@@ -1,35 +1,34 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
-
+import _ from "underscore";
 
 export default class TextListGroupWithHeading extends React.Component {
   render() {
     let { heading, property, thing } = this.props;
-    let truth = thing[property];
-    let truthString = String(truth);
-    if (!thing[property] || thing[property].length === 0) {
+    let value = thing[property];
+    if (!value || (_.isArray(value) && !value.length)) {
       return <div />;
-    } else if (thing[property] && thing[property].length === 1){
+    } else if (_.isArray(thing[property])) {
+      let items = thing[property].map(item => (
+        <div key={item.value}>{item.text}</div>
+      ));
       return (
         <div>
           <p className="sub-sub-heading">
-            <FormattedMessage id={property  ? property  : "not_specified"} />
+            <FormattedMessage id={property} />
           </p>
-          <div className={property + " blond"}><FormattedMessage id={truthString}/></div>
+          <div className={property}>{items}</div>
         </div>
       );
-    } else if (thing[property] || thing[property].length > 1){
-      let items = thing[property].map(item => (
-        <div key={item}>
-          {item}
-        </div>
-      ));
+    } else {
       return (
         <div>
           <p className="sub-sub-heading">
             <FormattedMessage id={property ? property : "not_specified"} />
           </p>
-          <div className={property}>{items}</div>
+          <div className={property + " blond"}>
+            <FormattedMessage id={value} />
+          </div>
         </div>
       );
     }
