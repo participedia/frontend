d8f26327dc65fad820441f70129bfeadc16b2916
Alanna Scott <alanna.scott@gmail.com>
2019-03-15 15:36:44 -0700

    add comment for context

--- src/components/TextListWithHeading.js
+++ src/components/TextListWithHeading.js
@@ -15,6 +15,16 @@ export default class TextListGroupWithHeading extends React.Component {
       return null;
     } else if (_.isArray(value)) {
       if (property === "has_components" || property === "process_methods") {
+        // api.participedia.xyz returns multiple entries for a single id for these properties
+        // ie: value = [ {id: 1, text: 'text'}, {id: 1, text: "edited text"} ]
+        // deDupeThings function will filter the entries by id and select the last one,
+        // so we aren't rendering duplicates
+
+        // this should be fixed in the api repo rather than in the frontend repo,
+        // but we have started a new site/api on the participedia/api repo
+        // and it has diverged significantly.
+        // see issue here: https://github.com/participedia/api/issues/464
+
         value = deDupeThings(value);
       }
       let items = value.map(item => (
